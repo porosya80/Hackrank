05b48adbbf68061a4d59a46ec4c1efa5f9047cc8
Dmytriy Myk <porosya@gmail.com>
2019-06-29 22:14:20 +0300

    it is work

--- AlphabetRangoli.py
+++ AlphabetRangoli.py
@@ -1,27 +1,19 @@
 def print_rangoli(size):
     rows = []
-    letters = [chr(i+97) for i in range(size-1,-1,-1)]
-    base = "-".join(letters)
+    letters = "-".join([chr(i + 97) for i in range(size - 1, -1, -1)])
 
     for i in range(size):
-        row = base[:len(base) - i*2]
-        row = ("-" * (len(base) - len(row))) + row
-        rows.insert(0, row)
+        rows.insert(0, ("-" * (i * 2)) + letters[: len(letters) - i * 2])
 
     rows.extend(reversed(rows[:-1]))
 
-    for i in range(len(rows)):
-        row = rows[i]
+    for i, row in enumerate(rows):
         rev_row = row[::-1]
         rows[i] = row + rev_row[1:]
     print(*rows, sep="\n")
 
 
+if __name__ == "__main__":
+    n = int(input())
 
-
-
-
-if __name__ == '__main__':
-    # n = int(input())
-    n = 3
     print_rangoli(n)
---
+++ capitalize.py
@@ -0,0 +1,8 @@
+# Complete the solve function below.
+def solve(s):
+    return " ".join([word.capitalize() for word in s.split()])
+
+
+
+
+print(solve("132 456 Wq  m e"))
